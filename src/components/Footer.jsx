f7f753458d884496666f5b91d229091da85c5c0f
Rimmer-23 <rimmerfaceit2@gmail.com>
2025-10-28 16:59:42 +0100

    Add linkedIn link in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,15 @@
 export function Footer() {
-    return(
+    return (
         <footer>
-                <div className="container">
-                    <p>Visualization Tool. All rights reserved.</p>
-                </div>
-            </footer>
+            <div className="container">
+                <p>Visualization Tool</p>
+                <p>
+                    GitHub: <a href="https://github.com/Rimmer-23">Rimmer-23</a>
+                </p>
+                <p>
+                    LinkedIn: <a href="https://www.linkedin.com/in/mykhailo-hryhoriev">Mykhailo Hryhoriev</a>
+                </p>
+            </div>
+        </footer>
     )
 }
