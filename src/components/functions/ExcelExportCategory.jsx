7572fa5e383887889997532a6b7f6e313da0cff1
Rimmer-23 <rimmerfaceit2@gmail.com>
2025-11-11 15:19:01 +0100

    feat: Add CSS styling for Excel export button

--- src/components/functions/ExcelExportCategory.jsx
+++ src/components/functions/ExcelExportCategory.jsx
@@ -1,6 +1,7 @@
 import * as XLSX from 'xlsx';
 import { saveAs } from 'file-saver';
 import { decode } from "he";
+import './ExcelExportCategory.css'
 
 export function ExcelExportCategory({chartData = [], selectedCategory, questions = []}) {
 
